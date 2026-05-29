9480e8cb945efd6431b5f9bb3e92f7f791be048a
JakirHossain <jakir.devbd@gmail.com>
2026-05-29 21:55:02 +0600

    some update done

--- client/src/api/http.ts
+++ client/src/api/http.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError, type InternalAxiosRequestConfig } from 'axios'
 
-const API_BASE_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:5000/api'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:5000/api'
 
 type RetriableRequestConfig = InternalAxiosRequestConfig & {
   _retry?: boolean
